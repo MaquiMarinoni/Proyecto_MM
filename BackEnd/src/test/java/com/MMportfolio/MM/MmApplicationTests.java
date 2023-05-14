67cc764ed28d9813b97fe34e740d242fc8ede5fb
macarena <marinonimacarena@gmail.com>
2023-05-13 21:39:40 -0300

    BackEnd
    creacion carpeta  backend

---
+++ BackEnd/src/test/java/com/MMportfolio/MM/MmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MMportfolio.MM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
